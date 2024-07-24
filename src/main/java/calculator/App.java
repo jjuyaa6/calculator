042b06f43f2a54298d1b9f91b3bc86aaa2cc181c
jjuyaa6 <jjuyaa6@gmail.com>
2024-07-24 16:39:20 +0900

    Level1 -1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,27 @@
 package calculator;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println("dddd");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("양수(0 포함) 2개를 입력하세요: ");
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if (num1 >= 0) {
+            System.out.println("첫 번째 숫자: " + num1);
+        } else {
+            System.out.println("첫 번째 숫자: 양수가 아닙니다");
+        }
+
+        if (num2 >= 0) {
+            System.out.println("두 번째 숫자: " + num2);
+        } else {
+            System.out.println("두 번째 숫자: 양수가 아닙니다");
+        }
+
     }
 }
