2324ea50b4bdf227b7a207edce5a3a7be1f73c2c
jjuyaa6 <jjuyaa6@gmail.com>
2024-07-25 23:35:24 +0900

    Level 1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,30 +3,56 @@ package calculator;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("양수(0 포함) 2개를 입력하세요: ");
-
+        //  Level 1 - 1
+        System.out.print("첫 번째 숫자를 입력하세요 : ");
         int num1 = sc.nextInt();
+        if (num1 < 0) {
+            System.out.print("양수(0 포함)를 입력하세요 : ");
+            num1 = sc.nextInt();
+        }
+        System.out.print("두 번째 숫자를 입력하세요 : ");
         int num2 = sc.nextInt();
-
-        if (num1 >= 0) {
-            System.out.println("첫 번째 숫자: " + num1);
-        } else {
-            System.out.println("첫 번째 숫자: 양수가 아닙니다");
+        if (num2 < 0) {
+            System.out.print("양수(0 포함)를 입력하세요 : ");
+            num2 = sc.nextInt();
         }
 
-        if (num2 >= 0) {
-            System.out.println("두 번째 숫자: " + num2);
-        } else {
-            System.out.println("두 번째 숫자: 양수가 아닙니다");
+        //  Level 1 - 2
+        System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /) : ");
+        /*  charAt() 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index)를 받는다.  */
+        char operator = sc.next().charAt(0);
+
+        //  Level 1 - 3
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.print("나눗셈 연산에서 분모(두번째 숫자)에 0이 입력될 수 없어, 다시 입력해 주세요 : ");
+                    num2 = sc.nextInt();
+                    result = num1 / num2;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.print("사칙 연산자가 아닙니다. 다시 입력해 주세요 : ");
+                operator = sc.next().charAt(0);
+                break;
         }
-
-        //1-2
-        System.out.println("사칙연산 기호를 입력하세요: ");
-
-        char sign = sc.next().charAt(0);
-        System.out.println(sign);
+        System.out.println("결과 : " + num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
